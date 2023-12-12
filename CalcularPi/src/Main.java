0466c60a684a42e510462da2a2c195398b5ef97a
m-rero <86166943+m-rero@users.noreply.github.com>
2023-12-12 14:38:51 -0300

    Update Main.java

--- CalcularPi/src/Main.java
+++ CalcularPi/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         calculadora.calcularPi();
-        calculadora.chegarValor("3.14159");
+        calculadora.chegarValor();
     }
 }
